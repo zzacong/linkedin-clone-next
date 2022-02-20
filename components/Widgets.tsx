1ca4a60010a65641b8acf95cd4568ae225181e7c
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-02-20 19:19:17 +1100

    tutorial: build feed page grid

---
+++ components/Widgets.tsx
@@ -0,0 +1,3 @@
+export default function Widgets() {
+  return <div className="">WIDGETS</div>
+}
--- pages/feed/index.tsx
+++ pages/feed/index.tsx
@@ -1,14 +1,36 @@
 import type { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import Header from '$components/Header'
+import Sidebar from '$components/Sidebar'
+import Posts from '$components/Posts'
+import Widgets from '$components/Widgets'
 
 export default function Feed() {
   return (
-    <div>
+    <div className="h-screen overflow-y-scroll bg-[#f3f2ef] dark:bg-black dark:text-white">
       <Head>
         <title>Feed | LinkedIn | Next</title>
       </Head>
       <Header />
+
+      <div className="px-4">
+        <div className="mx-auto my-6 grid max-w-xl grid-cols-12 gap-5 md:max-w-3xl lg:max-w-6xl">
+          {/* Sidebar */}
+          <div className="col-span-12 h-48 bg-red-100 md:col-span-4 lg:col-span-2">
+            <Sidebar />
+          </div>
+
+          {/* Feed */}
+          <main className="col-span-12 h-48 bg-red-100 md:col-span-8 lg:col-span-6">
+            <Posts />
+          </main>
+
+          {/* Widgets (aside) */}
+          <aside className="hidden h-48 bg-red-100 lg:col-span-4 lg:block">
+            <Widgets />
+          </aside>
+        </div>
+      </div>
     </div>
   )
 }
