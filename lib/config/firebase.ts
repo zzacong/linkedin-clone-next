4b94a52a433e770bc1648e9c193ab01d5ba5d32d
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-02-25 20:23:12 +1100

    feat: firebase config

--- lib/config/firebase.ts
+++ lib/config/firebase.ts
@@ -1,14 +1,7 @@
 import { initializeApp, getApps, getApp } from 'firebase/app'
 import { getStorage } from 'firebase/storage'
 
-const config = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-}
+const config = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG ?? '{}')
 
 export const app = getApps().length ? getApp() : initializeApp(config)
 export const storage = getStorage()
