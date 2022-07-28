3f10cae6007655d0414e8649547bcb5050f240a1
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-07-28 23:09:43 +1000

    downgrade next to fix next auth getToken

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,20 +2,23 @@ import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 import { getToken } from 'next-auth/jwt'
 
+const secret = process.env.NEXTAUTH_SECRET
+
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req })
-  const isLoggedIn = !!token
+  const session = await getToken({ req, secret })
 
   if (
     (req.nextUrl.pathname.startsWith('/auth') ||
       req.nextUrl.pathname === '/') &&
-    isLoggedIn
+    session
   ) {
     return NextResponse.redirect(new URL('/feed', req.url))
   }
 
-  if (req.nextUrl.pathname.startsWith('/feed') && !isLoggedIn)
+  if (req.nextUrl.pathname.startsWith('/feed') && !session)
     return NextResponse.redirect(new URL('/', req.url))
+
+  return NextResponse.next()
 }
 
 export const config = {
